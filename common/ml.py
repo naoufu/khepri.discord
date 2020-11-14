5b4aa2b0762e730993378ed8609779d44c7384f7
naoufu <naoufuf@gmail.com>
2020-11-14 15:14:46 +0000

    vanilla commit

---
+++ common/ml.py
@@ -0,0 +1,37 @@
+import pickle
+from typing import Tuple
+from spacy.tokens import Doc
+import numpy as np
+np.seterr(divide='ignore', invalid='ignore') # Ignore error below
+# RuntimeWarning: divide by zero encountered in log
+#  preds = np.log(preds) / temperature
+import os
+
+
+def temp(p, temperature=1.0):
+    preds = np.asarray(p).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    index = np.argmax(probas)
+    return index
+
+
+def one_hot(idx: int, size: int):
+    ret = [0]*size
+    ret[idx] = 1
+    return ret
+
+
+class MLDataPreprocessor(object):
+    def __init__(self, name: str):
+        self.name = name
+        self.data = []
+        self.labels = []
+
+    def get_preprocessed_data(self) -> Tuple:
+        pass
+
+    def preprocess(self, doc: Doc) -> bool:
+        pass
